2477747359ee5ae43d79adf61a096d886f5f504d
Andre <29295439+pakueamb@users.noreply.github.com>
2018-09-23 10:55:37 -0400

    Update alarm_motion_buttons.py

--- alarm_motion_buttons.py
+++ alarm_motion_buttons.py
@@ -9,34 +9,29 @@ import time ; import datetime
 import os
 GPIO.setmode(GPIO.BCM)
 
-<<<<<<< HEAD
-GPIO.setup(21,GPIO.OUT) # setting the pins for lights as outputs
-=======
 all = (21, 23, 16, 12, 25, 24) #all lights
 GPIO.setup(all,GPIO.OUT) # setting the pins for lights as outputs
->>>>>>> d4f6827386eb9d65b40691616546ecee74eabc92
-
 GPIO.setup(20, GPIO.IN, pull_up_down = GPIO.PUD_UP)  # setting the pins for switches as inputs with pull up resistors
 GPIO.setup(27, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_DOWN) # setting up the PIR motion sensor as input with pull down resistor
 
 pygame.mixer.init() #initializing pygame.mixer
-alarm = pygame.mixer.Sound("/home/pi/Documents/alarm.wav") #creating the sound file for the alarm
+alarm = pygame.mixer.Sound("/Documents/alarm.wav") #creating the sound file for the alarm
 #In python 3, you need to replace 'Documents/alarm.wav' by the full path on\f the file 'alarm.wav
 
 def countdown(t): # function for countdown before arming the alarm
     while t>=0:
         print (t)
         t -= 1 # means t = t - 1
-        GPIO.output(21,GPIO.HIGH)
+        GPIO.output(all,GPIO.HIGH)
         time.sleep(0.2)
-        GPIO.output(21,GPIO.LOW)
+        GPIO.output(all,GPIO.LOW)
         time.sleep(0.8)
     
 try:
     os.system('clear') #from the command line, use the command 'clear'
-    GPIO.output(21,GPIO.LOW)
+    GPIO.output(all,GPIO.LOW)
     print ("***Raspberry Pi Passive Infrared Alarm***\n")
     while 1:
         print ("    To set the alarm, press Key 1")
@@ -45,14 +40,14 @@ try:
         countdown(10)
         print ("Motion sensor enabled")
         GPIO.wait_for_edge(18, GPIO.RISING) #wait for a signal from the PIR motion sensor to keep going
-        GPIO.output(21,GPIO.HIGH) #turn on light
+        GPIO.output(all,GPIO.HIGH) #turn on light
         alarm.play(loops =-1) #plays the sound file in an endless loop
         #.play() plays sound files in the background of the code.
         triggered_time = datetime.datetime.now() #register the time alarm was trigerred
         print ("!!!INTRUDER DETECTED!!!\n!!!Alarm Triggered!!!\n")
         print ("To disable the alarm, press Key 2")
         GPIO.wait_for_edge(27, GPIO.FALLING) #wait for the signal from Key 2 (pin 27) to keep going
-        GPIO.output(21,GPIO.LOW) #turn off light
+        GPIO.output(all,GPIO.LOW) #turn off light
         alarm.stop()# stop playback
         print ("Alarm disabled\n\n")
         print ("Alarm trigerred at " + str(triggered_time.strftime("%Y/%m/%d %H:%M:%S")) + "\n") # gives trigerred time with a format
@@ -62,6 +57,6 @@ try:
         os.system('clear')
 
 except KeyboardInterrupt: # exit if Ctrl+C
-    GPIO.output(21,GPIO.LOW)
+    GPIO.output(all,GPIO.LOW)
     GPIO.cleanup() # clean pins
     print ("\n***bye***\n")
